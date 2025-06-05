eac793f55753e50a1e20664f8de07406ecc56ce9
Taysir Al Daqrouq <aldaqrouqtaysir@gmail.com>
2025-06-05 16:35:56 +0400

    added a line chart and a button to make it pretty

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import joblib
 import pandas as pd
 
+date_counts = df_with_date['datetime'].value_counts().sort_index()
+
 model = joblib.load("healthriskscore_model.pkl")
 
 st.title("Comparing Health Risk Score Across Two Cities")
@@ -19,12 +21,18 @@ city2 = st.selectbox(
     index=1
 )
 
-start_date = st.date_input("Start date (must be in September)", datetime(2024, 9, 1), min_value=datetime(2024, 9, 1))
-end_date = st.date_input("End date (must be in September)", datetime(2024, 9, 14), max_value=datetime(2024, 9, 30))
+start_date = st.date_input("Start date (must be in September)",
+                           datetime(2024, 9, 7),
+                           min_value=datetime(2024, 9, 7),
+                           max_value=datetime(2024, 9, 21))
+end_date = st.date_input("End date (must be in September)",
+                         datetime(2024, 9, 14),
+                         min_value=datetime(2024, 9, 7),
+                         max_value=datetime(2024, 9, 21))
 
-if start_date.month != 9 or end_date.month != 9:
-    st.warning("Please make sure the date is in September or the result may be inaccurate!")
-else:
+x = st.button(f"Compare {city1} and {city2}")
+
+if x:
     start_date = pd.to_datetime(start_date)
     end_date = pd.to_datetime(end_date)
 
@@ -56,24 +64,31 @@ else:
         # sum for values that are accumulative throughout the day
 
         X = city_averages
+        dates = city_averages.index.tolist()
 
-        return X
+        return X, dates
 
-    inputs1 = data_filtering(df_with_date, code1, start_date, end_date)
-    inputs2 = data_filtering(df_with_date, code2, start_date, end_date)
+    inputs1, dates1 = data_filtering(df_with_date, code1, start_date, end_date)
+    inputs2, dates2 = data_filtering(df_with_date, code2, start_date, end_date)
 
-    print(inputs1)
     pred1 = model.predict(inputs1)
     pred2 = model.predict(inputs2)
 
-    print(pred1)
+    avg_score_1 = sum(pred1) / len(pred1)
+    avg_score_2 = sum(pred2) / len(pred2)
 
-    #avg1_score = pred1[0]
-    #avg2_score = pred2[0]
+    line_chart_data = pd.DataFrame({
+        f'{city1}': pred1,
+        f'{city2}': pred2
+    }, index = dates1)
 
-    #st.markdown(f"Average Health Risk:")
-    #st.markdown(f"- {city1}: `{avg1_score:.2f}`")
-    #st.markdown(f"- {city2}: `{avg2_score:.2f}`")
+    st.subheader("Health Risk Score Over Time")
 
-    #winner = city1 if avg1_score < avg2_score else city2
-    #st.success(f"{winner} is healthier during this time period.")
+    st.line_chart(line_chart_data)
+
+    st.write(f"Average Health Risk:")
+    st.write(f"- {city1}: `{avg_score_1:.2f}`")
+    st.write(f"- {city2}: `{avg_score_2:.2f}`")
+
+    winner = city1 if avg_score_1 < avg_score_2 else city2
+    st.write(f"{winner} is healthier during this time period.")
